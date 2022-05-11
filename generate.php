2b836c5b6fb5aa88cf412a9bf104cf7e39b85f4d
Philipp Hermes <philipphermes2001@gmail.com>
2022-05-11 18:35:43 +0200

    add more parameters

--- generate.php
+++ generate.php
@@ -17,6 +17,14 @@ $parameters = [
         'name' => 'username',
         'type' => 'string'
     ],
+    1 => [
+        'name' => 'email',
+        'type' => 'string'
+    ],
+    2 => [
+        'name' => 'password',
+        'type' => 'string'
+    ],
 ];
 
 $path = sprintf('%s/%s.php', $generatedDTOPath, $fileName);
@@ -26,12 +34,16 @@ $namespace = str_replace(['src', '/'], ['App', '\\'], $generatedDTOPath);
 $base = sprintf("<?php\n\nnamespace %s;\n\nclass %s \n{\n", $namespace, $fileName);
 
 foreach ($parameters as $parameter) {
-    $base .= sprintf("\tprivate %s \$%s;", $parameter['type'], $parameter['name']);
+    $base .= sprintf("\tprivate %s \$%s;\n\n", $parameter['type'], $parameter['name']);
 }
 
-foreach ($parameters as $parameter) {
-    $base .= sprintf("\n\n\tpublic function get%s(): %s\n\t{\n\t\treturn \$this->%s;\n\t}", ucfirst($parameter['name']), $parameter['type'], $parameter['name']);
-    $base .= sprintf("\n\n\tpublic function set%s(%s \$%s): self\n\t{\n\t\t\$this->%s = \$%s;\n\n\t\treturn \$this;\n\t}", ucfirst($parameter['name']), $parameter['type'], $parameter['name'], $parameter['name'], $parameter['name']);
+foreach ($parameters as $key => $parameter) {
+    $base .= sprintf("\tpublic function get%s(): %s\n\t{\n\t\treturn \$this->%s;\n\t}\n\n", ucfirst($parameter['name']), $parameter['type'], $parameter['name']);
+    $base .= sprintf("\tpublic function set%s(%s \$%s): self\n\t{\n\t\t\$this->%s = \$%s;\n\n\t\treturn \$this;\n\t}", ucfirst($parameter['name']), $parameter['type'], $parameter['name'], $parameter['name'], $parameter['name']);
+
+    if(($key+1) !== count($parameters)) {
+        $base .= "\n\n";
+    }
 }
 
 $base .= "\n}";
--- src/GeneratedDataTransferObjects/TestDTO.php
+++ src/GeneratedDataTransferObjects/TestDTO.php
@@ -6,6 +6,10 @@ class TestDTO
 {
 	private string $username;
 
+	private string $email;
+
+	private string $password;
+
 	public function getUsername(): string
 	{
 		return $this->username;
@@ -14,6 +18,30 @@ class TestDTO
 	public function setUsername(string $username): self
 	{
 		$this->username = $username;
+
+		return $this;
+	}
+
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+
+	public function setEmail(string $email): self
+	{
+		$this->email = $email;
+
+		return $this;
+	}
+
+	public function getPassword(): string
+	{
+		return $this->password;
+	}
+
+	public function setPassword(string $password): self
+	{
+		$this->password = $password;
 
 		return $this;
 	}
